d32d6e7cf98913cd53addaae5c52a6d2f1474146
Adam Ketchum <ketchua@purdue.edu>
2022-10-24 20:56:51 -0400

    first test with google cloud and sqlalchemy

---
+++ test.py
@@ -0,0 +1,32 @@
+from ctypes import resize
+from google.cloud.sql.connector import Connector
+import sqlalchemy
+from sqlalchemy.engine import result_tuple
+
+def getconn():
+  result = connector.connect(
+    "cs348prj:us-central1:cs348prj",
+    "pymysql",
+    user="root",
+    password="cs348",
+    db="db1"
+  )
+  return result
+
+
+if __name__ == '__main__':
+  connector = Connector();
+  pool = sqlalchemy.create_engine( "mysql+pymysql://", creator=getconn,)
+  
+  data = [('asdf', 2), ('stuff', 3), ('jgrfhk', 4), ('rjrkj', 1), ('adam', 40), ('neha', 7), ('test', 69), ('data', 10)]
+
+  with pool.connect() as db_conn:
+    #db_conn.execute('CREATE TABLE test (data varchar(255), id int)')
+    #for d in data:
+    #  db_conn.execute(f'INSERT INTO test VALUE ("{d[0]}", {d[1]})')
+    result = db_conn.execute("SELECT * FROM test").fetchall()
+
+    for row in result:
+      print(result)
+
+  connector.close()
