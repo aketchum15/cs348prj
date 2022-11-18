f113b70f2576ba98919e5373548510b261857b86
Adam Ketchum <ketchua@purdue.edu>
2022-11-17 21:45:30 -0500

    search getting closer

--- cs348VenueManager/venue_manager_prj/venue_manager_app/views.py
+++ cs348VenueManager/venue_manager_prj/venue_manager_app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import ListView
 from django.db.models import Q
 
@@ -13,19 +12,28 @@ def home_page(request):
     return render(request, 'vm_home_page.html')
 
 class search_results(ListView):
-    model = Performers
     template_name = 'search.html'
 
+
     def get_queryset(self):
-        query = self.request.GET.get('search')
         table = getattr(my_models, self.request.GET.get('table'))
+        query = self.request.GET.get('search')
         if hasattr(table, 'name'):
             object_list = table.objects.filter(Q(name__icontains=query))
         else:
             object_list = table.objects.filter(Q(id__icontains=query))
 
-        for item in object_list:
-            print(item)
-
-
         return object_list
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        table = getattr(my_models, self.request.GET.get('table'))
+        context = super().get_context_data(object_list=object_list, **kwargs)
+
+        context['table'] = table
+
+        if (hasattr(table, 'name')):
+            context['attrs'] = [field.name for field in table._meta.get_fields()]
+        else:
+            context['attrs'] = ['id']
+
+        return context
