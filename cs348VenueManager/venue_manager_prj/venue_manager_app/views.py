3b1003d5cd071d58e723de4705d3c53a275c2500
nehaha4490 <nehasunil1137@gmail.com>
2022-12-11 20:36:18 -0500

    more purchasing tickets stuff

--- cs348VenueManager/venue_manager_prj/venue_manager_app/views.py
+++ cs348VenueManager/venue_manager_prj/venue_manager_app/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import ListView
 from django.shortcuts import render 
 from django.db.models import Q
+from django.contrib import messages
+
 
 from . import models as my_models
 from .models import *
@@ -45,18 +47,33 @@ def purchase_tickets_page(request):
     return render(request, 'purchase_tickets.html', context=context)
 def purchase_tickets(request):
     #getting the information from form
-    name = request.GET('fname')
-    perfname = request.GET('shows')
+    name = request.GET.get('fname')
+    perfname = request.GET.get('shows')
     #getting information from the tables using the form information
-    table = my_models.Attendees.Meta.indexes
+    table = my_models.Attendees
     table2 = my_models.Performers
     table3 = my_models.Shows
     object_list = table.objects.filter(Q(name__icontains=name))
     attendee_id = object_list[0]
     object_list2 = table2.objects.filter(Q(name__icontains=perfname))
-    performer_id = object_list2[0]
+    performer_id = object_list2[1]
     object_list3 = table3.objects.filter(Q(id__icontains=performer_id))
-    show_id = object_list3[0]
+    showid = object_list3[1]
+
+
+    #transaction for checking show availability
+        #get capacity from shows table with show id
+    obj_list = table3.objects.filter(Q(show_id__icontains=showid))
+    capacity = obj_list[4]
+        #get count of tickets purchsed from Tickets table with show id
+    table4 = my_models.Tickets
+    obj_list2 = table4.objects.filter(Q(show_id__icontains=showid))
+    numtix = obj_list2[1]
+        #check if capacity = num of tickets purchased, if yes, dont allow purchase
+
     #inserting row into tickets table
-    insert_row = Tickets(attendee_id=attendee_id, show_id=show_id)
+    insert_row = Tickets(attendee_id=attendee_id, show_id=showid)
     insert_row.save()
+
+
+
